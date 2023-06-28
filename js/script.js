b09a7a2fb867b89d96e263a329a7be025b7659ec
Whillian de Sousa <135404385+Whillian-Sousa@users.noreply.github.com>
2023-06-28 01:31:02 -0300

    add

--- js/script.js
+++ js/script.js
@@ -35,14 +35,26 @@ if (navMenu && navCloseBtn) {
 
 
 // Change header bg color
+window.addEventListener("scroll", () => {
+    const scrollY = window.pageYOffset
 
+    if(scrollY > 5){
+        document.querySelector("header").classList.add("header-active")
+    } else {
+        document.querySelector("header").classList.remove("header-active")
+    }
 
 // Scroll up button
-  
-  
-  
+const scrollUpBtn = document.querySelector('.scrollUp-btn')
+
+if(scrollY > 250){
+    scrollUpBtn.classList.add("scrollUpBtn-active")
+} else {
+    scrollUpBtn.classList.remove("scrollUpBtn-active")
+}
+ 
 // Nav link indicator
  
-  
+})   
 // Scroll Reveal Animation
   
