682a9c36758ae753fd907ef079401249a6bb00af
Whillian de Sousa <135404385+Whillian-Sousa@users.noreply.github.com>
2023-06-28 16:38:56 -0300

    Finalizando site

--- js/script.js
+++ js/script.js
@@ -54,7 +54,35 @@ if(scrollY > 250){
 }
  
 // Nav link indicator
- 
+const sections = document.querySelectorAll('section[id]')
+
+sections.forEach(section =>{
+    const sectionHeight = section.offsetHeight,
+        sectionTop = section.offsetTop - 60
+
+        let navId = document.querySelector(`.menu-content a[href*= ${section.id}]`)
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            navId.classList.add("active-navlink")
+        } else {
+            navId.classList.remove("active-navlink")
+        }
+
+        navId.addEventListener("click", () => {
+            navMenu.classList.remove("open")
+            body.style.overflowY = "scroll"
+        })
+})
 })   
 // Scroll Reveal Animation
-  
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.section-title, .section-subtitle, .section-description, .brand-image, .testimonial, .newsletter, .logo-content, .newsletter-inputBox, .newsletter-mediaIcon, .footer-content, .footer-links`, {interval:100,})
+
+sr.reveal(`.about-imageContent, .menu-items`, {origin: 'left'})
+sr.reveal(`.about-details, .time-table`, {origin: 'right'})
